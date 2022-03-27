Fixed zeropoint gradients.

bitsandbytes/autograd/_functions.py
@@ -124,7 +124,7 @@ class MatMul8bit(torch.autograd.Function):
 
     @staticmethod
     def forward(ctx, A, B, out=None, quant_type='vector', precision=[8, 8, 8], index=None, s=None, args=None):
-        gemm_func = torch.matmul if quant_type in ['row-zeropoint', 'zeropoint'] else F.igemm
+        gemm_func = torch.matmul if 'zeropoint' in quant_type else F.igemm
 
         if precision[0] != 8:
             with torch.no_grad():
@@ -132,13 +132,16 @@ class MatMul8bit(torch.autograd.Function):
         else:
             if len(B.shape) == 2: dim = 0
             else: dim = 1
-            if quant_type == 'row':
-                qA, SA = F.vectorwise_quant(A, dim=-1, quant_type='linear')
-            else:
-                qA, SA = F.vectorwise_quant(A, dim=-1, quant_type=quant_type)
-            qB, SB = F.vectorwise_quant(B, dim=dim, quant_type=quant_type)
-            iout = gemm_func(qA, qB)
+            qA, SA = F.vectorwise_quant(A, dim=-1, quant_type=quant_type)
 
+            if quant_type == 'row':
+                qB, SB = F.vectorwise_quant(B, dim=dim, quant_type='linear')
+            elif quant_type == 'row-zeropoint':
+                qB, SB = F.vectorwise_quant(B, dim=dim, quant_type='zeropoint')
+            else:
+                qB, SB = F.vectorwise_quant(B, dim=dim, quant_type=quant_type)
+
+            iout = gemm_func(qA, qB)
             output = F.vectorwise_mm_dequant(iout, SA, SB, A.dtype, quant_type)
 
         if A.requires_grad or B.requires_grad:
@@ -159,7 +162,7 @@ class MatMul8bit(torch.autograd.Function):
         precision = ctx.precision
         grad_A = grad_B = None
         grad_s = getattr(ctx, 'grad_s', None)
-        gemm_func = torch.matmul if quant_type in ['row-zeropoint', 'zeropoint'] else F.igemm
+        gemm_func = torch.matmul if 'zeropoint' in quant_type else F.igemm
 
         if B.requires_grad:
             if len(A.shape) == 3:
@@ -197,7 +200,10 @@ class MatMul8bit(torch.autograd.Function):
                         qgrad_output, S1 = F.vectorwise_quant(grad_output, dim=dims, quant_type=quant_type)
                     qA, S2 = F.vectorwise_quant(A, dim=dims, quant_type=quant_type)
                     igrad_B = gemm_func(qA.permute(permute_dim), qgrad_output)
-                    grad_B = F.vectorwise_mm_dequant(igrad_B, S2.permute(permute_dim), S1, grad_output.dtype, quant_type)
+                    if quant_type in ['linear', 'zeropoint']:
+                        grad_B = F.vectorwise_mm_dequant(igrad_B, S2, S1, grad_output.dtype, quant_type)
+                    else:
+                        grad_B = F.vectorwise_mm_dequant(igrad_B, S2.permute(permute_dim), S1, grad_output.dtype, quant_type)
 
         if A.requires_grad:
             if len(grad_output.shape) == 3: dims = [2]

bitsandbytes/functional.py
@@ -844,15 +844,19 @@ def vectorwise_quant(x, dim=1, quant_type='vector'):
     elif quant_type == 'zeropoint':
         dtype = x.dtype
         x = x.float()
-        qx = 255./(x.max() - x.min())
+        dyna = x.max() - x.min()
+        if dyna == 0: dyna = 1
+        qx = 255./dyna
         minx = x.min()
         zpx = torch.round(minx* qx)
         x = torch.round(qx*x - zpx) + zpx
         return x, qx
-    elif quant_type == 'row-zeropoint':
+    elif quant_type in ['vector-zeropoint', 'row-zeropoint']:
         dtype = x.dtype
         x = x.float()
-        qx = 255./(torch.amax(x, dim=dim, keepdim=True) - torch.amin(x, dim=dim, keepdim=True))
+        dyna = (torch.amax(x, dim=dim, keepdim=True) - torch.amin(x, dim=dim, keepdim=True))
+        dyna[dyna==0] = 1
+        qx = 255./dyna
         minx = torch.amin(x, dim=dim, keepdim=True)
         zpx = torch.round(minx* qx)
         x = torch.round(qx*x - zpx) + zpx
@@ -892,6 +896,16 @@ def vectorwise_mm_dequant(xq, S1, S2, dtype=torch.half, quant_type='vector'):
             x *= norm
         else:
             x *= norm
+        return x.to(dtype)
+    elif quant_type == 'vector-zeropoint':
+        x = xq.float()
+        if len(S1.shape) == 3 and len(x.shape) == 2: S1 = S1.squeeze(0)
+        if len(S2.shape) == 3 and len(x.shape) == 2: S2 = S2.squeeze(0)
+        if len(S1.shape) == 2:
+            x *= 1.0/S1
+        else:
+            x *= 1.0/S1
+        x *= 1.0/S2
         return x.to(dtype)
     elif quant_type == 'row':
         x = xq.float()

tests/test_functional.py
@@ -455,10 +455,10 @@ def test_minmax_igemm(seq_dim, hidden_dim, batch_dim, transpose):
         relerrs.append(relerr.mean().item())
         errs2.append(err2.mean().item())
         relerrs2.append(relerr2.mean().item())
-    #print(mean(errs))
-    #print(mean(relerrs))
-    #print(mean(errs2))
-    #print(mean(relerrs2))
+    print(mean(errs))
+    print(mean(relerrs))
+    print(mean(errs2))
+    print(mean(relerrs2))
     assert mean(errs) < 0.015
     assert mean(relerrs) < 0.3
 
@@ -604,28 +604,81 @@ def test_overflow():
 
 
 def test_zeropoint():
-    batch = 4
-    seq = 32
-    model = 16
-    hidden = 8*model
-    A = torch.rand(batch*seq, model, device='cuda').half()
-    B = torch.rand(model, hidden, device='cuda').half()
+    def min_max(x):
+        maxA = torch.amax(x, dim=1, keepdim=True)
+        minA = torch.amin(x, dim=1, keepdim=True)
+        midpoint = (maxA-minA)/2.0
+        dyna = 252/(maxA-minA)
+        #dyna *= 0.98
+        x = dyna*x
+        x = x - torch.round((dyna*(minA+midpoint)))
+        return x.to(torch.int8), minA, midpoint, dyna
+    batch = 2
+    seq = 2
+    model = 4
+    hidden = 2*model
+    #batch = 4
+    #seq = 2048
+    #model = 1024
+    #hidden = 8*model
+    A = torch.randn(batch*seq, model, device='cuda').half()-0.4
+    B = torch.nn.Parameter(torch.randn(model, hidden, device='cuda').half())
+
+    #A[0] = 0
+    #B[:, 0] = 0
+    #A = A*(A>0)
+    #A[0, 0] = 0
+    #A[0, 0] = 6.0
+
+    Ac, minA, midpoint, dyna = min_max(A)
+    #print(Ac[0, 0], 'zero')
+    #print(Ac, Ac.min(), Ac.max())
+    Bc, maxB = F.vectorwise_quant(B, quant_type='linear')
+    out = F.igemm(Ac, Bc)
+    out2 = torch.matmul(A,B)
+    offset = B.sum(0)*torch.round(dyna*(minA+midpoint))/dyna
+    out = out.float()
+    #print(out.shape, maxB.shape, scale.shape, offset.shape)
+    norm1 = maxB/127
+    C4 = (out/dyna)*norm1+offset
 
 
-    C1 = torch.matmul(A, B)
-    C2 = bnb.matmul(A, B, None, 'linear')
-    C3 = bnb.matmul(A, B, None, 'zeropoint')
+    B1 = torch.nn.Parameter(B.clone())
+    B2 = torch.nn.Parameter(B.clone())
+    B3 = torch.nn.Parameter(B.clone())
+    B4 = torch.nn.Parameter(B.clone())
+
+
+    C1 = torch.matmul(A, B1)
+    C2 = bnb.matmul(A, B2, None, 'linear')
+    C3 = bnb.matmul(A, B3, None, 'zeropoint')
+    C4 = bnb.matmul(A, B4, None, 'vector-zeropoint')
 
     err1 = torch.abs(C1-C2).mean().item()
     err2 = torch.abs(C1-C3).mean().item()
-    assert err1 > err2
+    err3 = torch.abs(C1-C4).mean().item()
+    print(err1, err2, err3)
+    #assert err1 > err2
 
-    C2 = bnb.matmul(A, B, None, 'row')
-    C3 = bnb.matmul(A, B, None, 'row-zeropoint')
+    loss1 = C1.mean()
+    loss2 = C2.mean()
+    loss3 = C3.mean()
+    loss4 = C4.mean()
 
-    err1 = torch.abs(C1-C2).mean().item()
-    err2 = torch.abs(C1-C3).mean().item()
-    assert err1 > err2
+    loss1.backward()
+    loss2.backward()
+    loss3.backward()
+    loss4.backward()
+
+    print(B.grad)
+    print(B1.grad)
+    print(B2.grad)
+    print(B3.grad)
+    print(B4.grad)
+    err1 = torch.abs(B1.grad-B2.grad).mean().item()
+    err2 = torch.abs(B1.grad-B3.grad).mean().item()
+    err3 = torch.abs(B1.grad-B4.grad).mean().item()
+    print(err1, err2, err3)
 
 
 