Refactored matmullt with state for correct accumulated backprop.

bitsandbytes/autograd/_functions.py
@@ -1,6 +1,8 @@
 import torch
 import bitsandbytes.functional as F
 
+from dataclasses import dataclass
+
 tensor = torch.Tensor
 
 class MatMul8bit(torch.autograd.Function):
@@ -91,14 +93,42 @@ mm_cublas = MatMul8bit.apply
 bmm_cublas = MatMul8bit.apply
 matmul_cublas = MatMul8bit.apply
 
+@dataclass
+class MatmulLtState:
+    CxB = None
+    SB = None
+    SCB = None
+
+    CxBt = None
+    SBt = None
+    CBt = None
+
+    subB = None
+    has_accumulated_gradients = False
+    threshold = 0.0
+
+    def reset_grads(self):
+        self.CxB = None
+        self.SB = None
+        self.SCB = None
+
+        self.CxBt = None
+        self.SBt = None
+        self.CBt = None
+
+        self.subB = None
+
+
 class MatMul8bitLt(torch.autograd.Function):
 
     @staticmethod
-    def forward(ctx, A, B, out=None, decomp_threshold=0.0, CB=None, return_CB=False):
+    def forward(ctx, A, B, out=None, state=MatmulLtState()):
         requires_gradA = A.requires_grad
         requires_gradB = B.requires_grad
         formatB = F.get_special_format_str()
         input_shape = A.shape
+
+        has_grad = (True if (getattr(B, 'grad', None) is not None) else False)
 
         if len(A.shape) == 3:
             output_shape = (A.shape[0], A.shape[1], B.shape[0])
@@ -106,9 +136,10 @@ class MatMul8bitLt(torch.autograd.Function):
         else:
             output_shape = (A.shape[0], B.shape[0])
 
-        CA, CAt, SCA, SCAt, coo_tensorA = F.double_quant(A, threshold=decomp_threshold)
+        CA, CAt, SCA, SCAt, coo_tensorA = F.double_quant(A, threshold=state.threshold)
         idx = None
-        if decomp_threshold > 0.0 and coo_tensorA is not None:
+
+        if state.threshold > 0.0 and coo_tensorA is not None:
             pass
             idx = torch.unique(coo_tensorA.colidx).long()
             CA[:, idx] = 0
@@ -122,46 +153,42 @@ class MatMul8bitLt(torch.autograd.Function):
         is_transposed = not B.is_contiguous() and B.shape[0] == B.stride(1)
         if is_transposed: B = B.contiguous()
 
-        if CB is not None:
-            CxB, SB, SCB, subB = CB
-            CBt = SCBt = None
-        else:
-            CB, CBt, SCB, SCBt, coo_tensorB = F.double_quant(B)
-            CxB, SB = F.transform(CB, to_order=formatB)
+        if not has_grad or state.CxB is None:
+            state.reset_grads()
+            CB, state.CBt, state.SCB, state.SCBt, coo_tensorB = F.double_quant(B)
+            state.CxB, state.SB = F.transform(CB, to_order=formatB)
             subB = None
 
-        out32, Sout32 = F.igemmlt(C32A, CxB, SA, SB)
-        output = F.mm_dequant(out32, Sout32, SCA, SCB)
+        out32, Sout32 = F.igemmlt(C32A, state.CxB, SA, state.SB)
+        output = F.mm_dequant(out32, Sout32, SCA, state.SCB)
 
-        if decomp_threshold > 0.0 and coo_tensorA is not None:
+        if state.threshold > 0.0 and coo_tensorA is not None:
             output += torch.matmul(subA, B[:, idx].t())
+
+        ctx.state = state
 
         ctx.formatB = formatB
         ctx.grad_shape = input_shape
         ctx.req_grads = [requires_gradA, requires_gradB]
-        ctx.decomp_threshold = decomp_threshold
 
         if requires_gradA or requires_gradB:
-            ctx.tensors = (CAt, CBt, subA)
-            ctx.tensor_states = (SCAt, SCBt, idx)
+            ctx.tensors = (CAt, subA)
+            ctx.tensor_states = (SCAt, idx)
         else:
             ctx.tensors = [None, None]
             ctx.tensor_states = (None, None)
             ctx.save_for_backward(None, None)
 
         clone_func = torch.clone if len(output_shape) == 3 else lambda x : x
-        if return_CB:
-            return (clone_func(output.view(output_shape)), CxB, SCB, subB or torch.empty(1))
-        else:
-            return clone_func(output.view(output_shape))
+        return clone_func(output.view(output_shape))
 
     @staticmethod
-    def backward(ctx, grad_output, CxB=None, SCB=None, subB=None):
+    def backward(ctx, grad_output):
         req_gradA, req_gradB = ctx.req_grads
-        CAt, CBt, subA = ctx.tensors
-        SCAt, SCBt, idx = ctx.tensor_states
+        CAt, subA = ctx.tensors
+        SCAt, idx = ctx.tensor_states
         formatB = ctx.formatB
-        decomp_threshold = ctx.decomp_threshold
+        state = ctx.state
 
         if len(grad_output.shape) == 3:
             grad_output = grad_output.view(-1, grad_output.shape[-1]).contiguous()
@@ -174,15 +201,15 @@ class MatMul8bitLt(torch.autograd.Function):
             C32grad, Sgrad = F.transform(Cgradt, 'col32', transpose=True)
             gradB32, SgradB32 = F.igemmlt(C32grad, CxAt, Sgrad, SAt)
             grad_B = F.mm_dequant(gradB32, SgradB32, SCgradt, SCAt)
-            if decomp_threshold > 0.0 and subA is not None:
+            if state.threshold > 0.0 and subA is not None:
                 grad_B[:, idx] += torch.matmul(grad_output.t(), subA)
 
         if req_gradA:
             C32grad, Sgrad = F.transform(Cgrad, 'col32')
-            CxBt, SBt = F.transform(CBt, to_order=formatB, transpose=True)
-            gradA32, SgradA32 = F.igemmlt(C32grad, CxBt, Sgrad, SBt)
-            grad_A = F.mm_dequant(gradA32, SgradA32, SCgrad, SCBt).view(ctx.grad_shape)
-
+            if state.CxBt is None:
+                state.CxBt, state.SBt = F.transform(state.CBt, to_order=formatB, transpose=True)
+            gradA32, SgradA32 = F.igemmlt(C32grad, state.CxBt, Sgrad, state.SBt)
+            grad_A = F.mm_dequant(gradA32, SgradA32, SCgrad, state.SCBt).view(ctx.grad_shape)
 
         return grad_A, grad_B, None, None, None, None, None
 
@@ -190,6 +217,9 @@ class MatMul8bitLt(torch.autograd.Function):
 matmul = MatMul8bitLt.apply
 
 
-def matmul(A : tensor, B : tensor, out : tensor=None, threshold : int=0.0, CB : tensor=None, return_CB : bool=False):
-    return MatMul8bitLt.apply(A, B, out, threshold, CB, return_CB)
+def matmul(A : tensor, B : tensor, out : tensor=None, state : MatmulLtState = None, threshold=0.0):
+    state = state or MatmulLtState()
+    if threshold > 0.0:
+        state.threshold = threshold
+    return MatMul8bitLt.apply(A, B, out, state)
 

bitsandbytes/nn/modules.py
@@ -51,30 +51,13 @@ class Linear8bit(nn.Linear):
 class Linear8bitLt(nn.Linear):
     def __init__(self, input_features, output_features, bias=True, threshold=0.0):
         super(Linear8bitLt, self).__init__(input_features, output_features, bias)
-        self.CxB = None
-        self.SB = None
-        self.SCB = None
-        self.subB = None
-        self.has_accumulated_gradients = False
-        self.threshold = threshold
+        state = bnb.MatmulLtState()
+        state.threshold = threshold
+        self.state = state
 
     def forward(self, x):
-        has_grad = (True if (getattr(self.weight, 'grad', None) is not None) else False)
-        if has_grad: self.has_accumulated_gradients = True
+        out = bnb.matmul(x, self.weight, state=self.state)
 
-        if self.training and not self.has_accumulated_gradients:
-            self.CxB, self.SB, self.SCB = None, None, None
-            out = bnb.matmul(x, self.weight, threshold=self.threshold)
-            if self.bias is not None:
-                out += self.bias.unsqueeze(0).expand_as(out)
-            return out
-        else:
-            if self.CxB is None:
-                (out, self.CxB, self.SCB, self.subB) = bnb.matmul(x, self.weight, return_CB=True, threshold=self.threshold)
-                self.SB = (self.weight.shape, bnb.functional.get_special_format_str())
-            else:
-                out = bnb.matmul(x, self.weight, CB=(self.CxB, self.SB, self.SCB, self.subB), threshold=self.threshold)
-
-            if self.bias is not None:
-                out += self.bias.unsqueeze(0).expand_as(out)
-            return out
+        if self.bias is not None:
+            out += self.bias.unsqueeze(0).expand_as(out)
+        return out

tests/test_autograd.py
@@ -173,7 +173,7 @@ def test_matmul(dim1, dim2, dim3, dim4, funcs, dtype, req_grad, transpose):
 
 
 n = 1
-k = 10
+k = 2
 dim1 = torch.randint(16,64, size=(n,)).tolist()
 dim2 = torch.randint(32,96, size=(n,)).tolist()
 dim3 = torch.randint(32,96, size=(n,)).tolist()

tests/test_modules.py
@@ -17,7 +17,7 @@ def get_args():
     args.use_8bit_training = 'full'
     return args
 
-def assert_all_approx_close(a, b, atol, rtol, count):
+def assert_all_approx_close(a, b, atol=1e-8, rtol=1e-5, count=10):
     idx = torch.isclose(a, b, rtol, atol)
     sumval = (idx==0).sum().item()
     if sumval > count:
@@ -327,8 +327,9 @@ def test_linear8bit():
 
 
 threshold = [0.0, 3.0]
-values = list(product(threshold))
-names = ['threshold_{0}'.format(*vals) for vals in values]
+#values = list(product(threshold))
+values = threshold
+names = ['threshold_{0}'.format(vals) for vals in values]
 @pytest.mark.parametrize("threshold", values, ids=names)
 def test_linear8bitlt_inference(threshold):
     l1 = bnb.nn.Linear8bitLt(32,64, threshold=threshold).cuda().half()
@@ -338,25 +339,45 @@ def test_linear8bitlt_inference(threshold):
         b1 = torch.randn(16, 8, 32, device='cuda').half()
         o1 = l1(b1)
         if i == 1:
-            assert l1.CxB is not None
-
+            assert l1.state.CxB is not None
 
 def test_linear8bitlt_accumulated_gradient():
-    l1 = bnb.nn.Linear8bitLt(32,64).cuda().half()
-    opt = bnb.optim.Adam8bit(l1.parameters())
+    l1 = torch.nn.Sequential(*[bnb.nn.Linear8bitLt(32,32).cuda().half() for i in range(2)])
+    l2 = torch.nn.Sequential(*[torch.nn.Linear(32,32).cuda().half() for i in range(2)])
+    l2[0].weight = torch.nn.Parameter(l1[0].weight.clone())
+    l2[0].bias = torch.nn.Parameter(l1[0].bias.clone())
+    l2[1].weight = torch.nn.Parameter(l1[1].weight.clone())
+    l2[1].bias = torch.nn.Parameter(l1[1].bias.clone())
+    opt1 = bnb.optim.Adam8bit(l1.parameters(), lr=0.001)
+    opt2 = bnb.optim.Adam8bit(l2.parameters(), lr=0.001)
 
     acc_steps = 10
 
-    for i in range(100):
+
+    for i in range(1000):
         b1 = torch.randn(16, 8, 32, device='cuda').half()
         o1 = l1(b1)
-        loss = o1.mean()
-        loss.backward()
+        o2 = l2(b1)
+        loss1 = o1.mean()
+        loss2 = o2.mean()
+        loss1.backward()
+        loss2.backward()
         if i == 2:
-            assert l1.CxB is not None
+            assert l1[0].state.CxB is not None
+            assert l1[1].state.CxB is not None
 
         if i > 0 and i % acc_steps == 0:
-            opt.step()
-            opt.zero_grad()
+            opt1.step()
+            opt1.zero_grad(True)
+            opt2.step()
+            opt2.zero_grad(True)
+            assert_all_approx_close(l1[0].weight, l2[0].weight, rtol=1.05, atol=0.01, count=2)
+            assert_all_approx_close(l1[1].weight, l2[1].weight, rtol=1.05, atol=0.01, count=2)
+            # we do this copy because otherwise we have small divergences over time that add up
+            l1[0].weight.data.copy_(l2[0].weight.data)
+            l1[1].weight.data.copy_(l2[1].weight.data)
+        else:
+            torch.testing.assert_allclose(l1[0].weight.grad, l2[0].weight.grad)
+            torch.testing.assert_allclose(l1[1].weight.grad, l2[1].weight.grad)
 
 