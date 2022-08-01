c4fe6c69a33bacd292dfab87dc91dad32ba5fcf4
Titus von Koeller <titus@vonkoeller.com>
2022-08-01 09:39:56 -0700

    deleted function that was moved but accidentally not removed in commit

--- bitsandbytes/cuda_setup.py
+++ bitsandbytes/cuda_setup.py
@@ -19,33 +19,11 @@ evaluation:
 """
 
 import ctypes
-import shlex
-import subprocess
 from os import environ as env
 from pathlib import Path
 from typing import Set, Union
 
 from .utils import print_err, warn_of_missing_prerequisite
-
-
-def execute_and_return(command_string: str) -> Tuple[str, str]:
-    def _decode(subprocess_err_out_tuple):
-        return tuple(
-            to_decode.decode("UTF-8").strip()
-            for to_decode in subprocess_err_out_tuple
-        )
-
-    def execute_and_return_decoded_std_streams(command_string):
-        return _decode(
-            subprocess.Popen(
-                shlex.split(command_string),
-                stdout=subprocess.PIPE,
-                stderr=subprocess.PIPE,
-            ).communicate()
-        )
-
-    std_out, std_err = execute_and_return_decoded_std_streams()
-    return std_out, std_err
 
 
 def check_cuda_result(cuda, result_val):
