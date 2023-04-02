600793e4534915049978e11aca01103da5524de6
Rickard <rly@combination.se>
2023-04-02 12:41:48 +0200

    Utility function for determining CUDA devices

--- bitsandbytes/utils.py
+++ bitsandbytes/utils.py
@@ -1,7 +1,7 @@
 import shlex
 import subprocess
 from typing import Tuple
-
+import torch
 
 def execute_and_return(command_string: str) -> Tuple[str, str]:
     def _decode(subprocess_err_out_tuple):
@@ -21,3 +21,17 @@ def execute_and_return(command_string: str) -> Tuple[str, str]:
 
     std_out, std_err = execute_and_return_decoded_std_streams(command_string)
     return std_out, std_err
+
+__cuda_devices = None
+def get_cuda_devices():
+    global __cuda_devices
+    if __cuda_devices is None:
+        cuda_devices = []
+        if torch.cuda.is_available():
+            devices = [d for d in range(torch.cuda.device_count())]
+            cuda_devices = [torch.cuda.get_device_name(d) for d in devices]
+        __cuda_devices = cuda_devices
+    return __cuda_devices
+
+def is_cuda_device(device):
+    return device in get_cuda_devices()
