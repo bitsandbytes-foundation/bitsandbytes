cb8d1f63d62a00c97271ddca6f49608fc700cbc9
Rickard <rickardp@users.noreply.github.com>
2023-04-18 07:44:57 +0200

    Add all extension types

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ import os
 from setuptools import find_packages, setup
 from setuptools.dist import Distribution
 
-libs = list(glob.glob("./bitsandbytes/libbitsandbytes*.so"))
+libs = list(glob.glob("./bitsandbytes/libbitsandbytes*.*"))
 libs = [os.path.basename(p) for p in libs]
 print("libs:", libs)
 
