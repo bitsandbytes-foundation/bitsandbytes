def1a324efd163f4a7343a85591620fd5f98e09c
dailingx <daixinliang.dxl@alibaba-inc.com>
2023-05-11 20:16:47 +0800

    When using sudo to run the command, the system variable environment is reset and the original environment variable cannot be obtained.

--- bitsandbytes/cuda_setup/env_vars.py
+++ bitsandbytes/cuda_setup/env_vars.py
@@ -53,6 +53,10 @@ def get_potentially_lib_path_containing_env_vars() -> Dict[str, str]:
     }
 
 def restore_original_system_env_vars(env_vars: Dict[str, str]):
+    """
+        Use the `sudo -i env` command to obtain the original environment variables of the system, including CONDA_PREFIX, LD_LIBRARY_PATH...
+        Then replace the environment variables obtained from the current shell, to obtain correct and complete environment variables
+    """
     output = subprocess.check_output(['sudo', '-i', 'env'])
     output_str = output.decode('utf-8')
     env_lines = output_str.split('\n')
