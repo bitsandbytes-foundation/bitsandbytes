f4996978db44102bc30b2e97820ab63d108c234c
Tim Dettmers <tim.dettmers@gmail.com>
2023-07-15 10:22:08 -0700

    Added missing check if LD_LIBRARY_PATH exists. #588

--- bitsandbytes/__main__.py
+++ bitsandbytes/__main__.py
@@ -63,15 +63,16 @@ def generate_bug_report_information():
         print('')
 
     print_header("LD_LIBRARY CUDA PATHS")
-    lib_path = os.environ['LD_LIBRARY_PATH'].strip()
-    for path in set(lib_path.split(':')):
-        try:
-            if isdir(path):
-                print_header(f"{path} CUDA PATHS")
-                paths = find_file_recursive(path, '*cuda*so')
-                print(paths)
-        except:
-            print(f'Could not read LD_LIBRARY_PATH: {path}')
+    if 'LD_LIBRARY_PATH' in os.environ:
+        lib_path = os.environ['LD_LIBRARY_PATH'].strip()
+        for path in set(lib_path.split(':')):
+            try:
+                if isdir(path):
+                    print_header(f"{path} CUDA PATHS")
+                    paths = find_file_recursive(path, '*cuda*so')
+                    print(paths)
+            except:
+                print(f'Could not read LD_LIBRARY_PATH: {path}')
     print('')
 
 
