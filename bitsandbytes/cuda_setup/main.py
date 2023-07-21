b4bc3369a0831269e82051ecbefec6b9262bba66
Keith Stevens <fozziethebeat@gmail.com>
2023-07-21 14:30:03 +0900

    Make sure bitsandbytes handles permission errors in the right order

--- bitsandbytes/cuda_setup/main.py
+++ bitsandbytes/cuda_setup/main.py
@@ -196,11 +196,13 @@ def remove_non_existent_dirs(candidate_paths: Set[Path]) -> Set[Path]:
         try:
             if path.exists():
                 existent_directories.add(path)
+        except PermissionError as pex:
+            # Handle the PermissionError first as it is a subtype of OSError 
+            # https://docs.python.org/3/library/exceptions.html#exception-hierarchy
+            pass
         except OSError as exc:
             if exc.errno != errno.ENAMETOOLONG:
                 raise exc
-        except PermissionError as pex:
-            pass
 
     non_existent_directories: Set[Path] = candidate_paths - existent_directories
     if non_existent_directories:
