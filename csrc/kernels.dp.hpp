75d9277ffc37b900ab8f8ba4b9b7de5b8de81c90
abhilash1910 <abhilash.majumder@intel.com>
2023-09-05 11:02:56 -0700

    init kernel headers

---
+++ csrc/kernels.dp.hpp
@@ -0,0 +1,212 @@
+// Copyright (c) Facebook, Inc. and its affiliates.
+//
+// This source code is licensed under the MIT license found in the
+// LICENSE file in the root directory of this source tree.
+
+#include <sycl/sycl.hpp>
+#include <dpct/dpct.hpp>
+#include <float.h>
+#include "ops.dp.hpp"
+
+#ifndef kernels
+#define kernels
+
+#pragma once
+
+//template <int QUANT_TYPE, typename INP_TYPE, typename COMP_TYPE, typename OUT_TYPE>__global__ void kMatmul_inference_4bit(INP_TYPE *A, unsigned char *B, OUT_TYPE *out, int lda, int ldb, int rowsA, int colsA, int colsB);
+
+template<typename T> extern SYCL_EXTERNAL void kEstimateQuantiles(T *__restrict__ const A, float *code, const float offset, const T max_val, const int n,
+                                            const sycl::nd_item<3> &item_ct1,
+                                            uint8_t *temp_storage_ct1);
+
+extern SYCL_EXTERNAL void kQuantize(float * code, float * __restrict__ const A, unsigned char *out, const int n,
+               const sycl::nd_item<3> &item_ct1, float *smem_code);
+extern SYCL_EXTERNAL void kDequantize(float *code, unsigned char *A, float *out, const int n,
+                 const sycl::nd_item<3> &item_ct1, float *smem_code);
+
+template<typename T, int BLOCK_SIZE, int NUM_PER_TH, int STOCHASTIC, int DATA_TYPE> extern SYCL_EXTERNAL void kQuantizeBlockwise(float * code, T * __restrict__ const A, float *absmax, unsigned char *out, float * __restrict__ const rand, const int rand_offset, const int n,
+                                                                                                            const sycl::nd_item<3> &item_ct1,
+                                                                                                            typename LoadT::TempStorage &loadt,
+                                                                                                            typename LoadFloat::TempStorage &loadf,
+                                                                                                            typename StoreChar::TempStorage &storec,
+                                                                                                            typename BlockReduce::TempStorage &reduce,
+                                                                                                            float *smem_code,
+                                                                                                            float *smem_absmax_value);
+template<typename T, int BLOCK_SIZE, int THREADS, int NUM_PER_TH, int DATA_TYPE> extern SYCL_EXTERNAL void kDequantizeBlockwise(float *code, unsigned char * A, float * absmax, T *out, const int blocksize, const int n,
+                                                                                                           const sycl::nd_item<3> &item_ct1,
+                                                                                                           typename LoadChar::TempStorage &loadchar,
+                                                                                                           typename StoreT::TempStorage &storet);
+
+template<typename T, int OPTIMIZER, int BLOCK_SIZE, int NUM_VALS>
+extern SYCL_EXTERNAL void kPreconditionOptimizer32bit2State(T* g, T* p,
+                float* state1, float* state2, float *unorm,
+                const float beta1, const float beta2, const float eps, const float weight_decay,
+                const int step, const float lr, const float gnorm_scale, const int n,
+                const sycl::nd_item<3> &item_ct1, uint8_t *temp_storage_ct1);
+
+template<typename T, int OPTIMIZER>
+extern SYCL_EXTERNAL void kOptimizer32bit2State(T* g, T* p,
+                float* state1, float* state2, float *unorm, const float max_unorm, const float param_norm,
+                const float beta1, const float beta2, const float eps, const float weight_decay,
+                const int step, const float lr, const float gnorm_scale, const bool skip_zeros, const int n,
+                const sycl::nd_item<3> &item_ct1, uint8_t *temp_storage_ct1);
+
+template<typename T, int OPTIMIZER, int BLOCK_SIZE, int NUM_VALS>
+extern SYCL_EXTERNAL void kPreconditionOptimizer32bit1State(T* g, T* p,
+                float* state1, float *unorm,
+                const float beta1, const float beta2, const float eps, const float weight_decay,
+                const int step, const float lr, const float gnorm_scale, const int n,
+                const sycl::nd_item<3> &item_ct1, uint8_t *temp_storage_ct1);
+
+template<typename T, int OPTIMIZER>
+extern SYCL_EXTERNAL void kOptimizer32bit1State(T* g, T* p,
+                float* state1,  float *unorm, const float max_unorm, const float param_norm,
+                const float beta1, const float beta2, const float eps, const float weight_decay,
+                const int step, const float lr, const float gnorm_scale, const bool skip_zeros, const int n,
+                const sycl::nd_item<3> &item_ct1, uint8_t *temp_storage_ct1);
+
+template<typename T, int OPTIMIZER>
+extern SYCL_EXTERNAL void
+kPreconditionOptimizerStatic8bit1State(T* p, T* __restrict__ const g, unsigned char*__restrict__  const state1,
+                float *unorm,
+                const float beta1, const float beta2,
+                const float eps, const int step,
+                float* __restrict__ const quantiles1,
+                float* max1, float* new_max1,
+                const float weight_decay,
+                const float gnorm_scale, const int n,
+                const sycl::nd_item<3> &item_ct1, uint8_t *temp_storage_ct1,
+                float *smem_quantiles1);
+
+
+template<typename T, int OPTIMIZER>
+extern SYCL_EXTERNAL void
+kOptimizerStatic8bit1State(T* p, T* const g, unsigned char* state1,
+                const float *unorm, const float max_unorm, const float param_norm,
+                const float beta1, const float beta2,
+                const float eps, const int step, const float lr,
+                float* __restrict__ const quantiles1,
+                float* max1, float* new_max1,
+                float weight_decay, const float gnorm_scale, const int n,
+                const sycl::nd_item<3> &item_ct1, float *smem_quantiles1,
+                uint8_t *temp_storage_ct1);
+
+
+
+template<typename T, int OPTIMIZER>
+extern SYCL_EXTERNAL void
+kPreconditionOptimizerStatic8bit2State(T* p, T* __restrict__ const g, unsigned char*__restrict__  const state1, unsigned char* __restrict__ const state2,
+                float *unorm,
+                const float beta1, const float beta2,
+                const float eps, const int step,
+                float* __restrict__ const quantiles1, float* __restrict__ const quantiles2,
+                float* max1, float* max2, float* new_max1, float* new_max2,
+                const float gnorm_scale, const int n,
+                const sycl::nd_item<3> &item_ct1, uint8_t *temp_storage_ct1,
+                float *smem_quantiles1, float *smem_quantiles2);
+
+
+template<typename T, int OPTIMIZER>
+extern SYCL_EXTERNAL void
+kOptimizerStatic8bit2State(T* p, T* const g, unsigned char* state1, unsigned char* state2,
+                const float *unorm, const float max_unorm, const float param_norm,
+                const float beta1, const float beta2,
+                const float eps, const int step, const float lr,
+                float* __restrict__ const quantiles1, float* __restrict__ const quantiles2,
+                float* max1, float* max2, float* new_max1, float* new_max2,
+                float weight_decay, const float gnorm_scale, const int n,
+                const sycl::nd_item<3> &item_ct1, float *smem_quantiles1,
+                float *smem_quantiles2, uint8_t *temp_storage_ct1);
+
+template<typename T, int OPTIMIZER, int BLOCK_SIZE, int N_PER_TH> extern SYCL_EXTERNAL void kOptimizerStatic8bit2StateBlockwise(
+		T* p, T* __restrict__ const g, unsigned char* state1, unsigned char* state2,
+                const float beta1, const float beta2, const float eps, const int step, const float lr,
+                float* __restrict__ const quantiles1, float* __restrict__ const quantiles2,
+                float* absmax1, float* absmax2, float weight_decay, const float gnorm_scale, const bool skip_zeros, const int n,
+                const sycl::nd_item<3> &item_ct1,
+                sycl::local_accessor<float, 2> smem_quantiles1,
+                sycl::local_accessor<float, 2> smem_quantiles2,
+                typename BlockReduce1::TempStorage &reduce1,
+                typename BlockReduce2::TempStorage &reduce2,
+                float *smem_exchange1, float *smem_exchange2,
+                uint8_t *temp_storage_ct1);
+
+template<typename T, int OPTIMIZER, int BLOCK_SIZE, int N_PER_TH> extern SYCL_EXTERNAL void kOptimizerStatic8bit1StateBlockwise(
+		T* p, T* __restrict__ const g, unsigned char* state1,
+                const float beta1, const float beta2,
+                const float eps, const int step, const float lr,
+                float* __restrict__ const quantiles1,
+                float* absmax1,
+                float weight_decay,
+                const float gnorm_scale, const bool skip_zeros, const int n,
+                const sycl::nd_item<3> &item_ct1,
+                sycl::local_accessor<float, 2> smem_quantiles1,
+                typename BlockReduce1::TempStorage &reduce1,
+                float *smem_exchange1, uint8_t *temp_storage_ct1);
+
+
+template<typename T, int BLOCK_SIZE, int NUM_VALS> extern SYCL_EXTERNAL void kPercentileClipping(T * __restrict__ g, float *gnorm_vec, int step, const int n,
+                                                                            const sycl::nd_item<3> &item_ct1,
+                                                                            typename BlockReduce::TempStorage &reduce,
+                                                                            typename LoadT::TempStorage &loadT);
+
+
+void kHistogramScatterAdd2D(float* histogram, int *index1, int *index2, float *src, const int maxidx1, const int n,
+                            const sycl::nd_item<3> &item_ct1);
+
+template <typename T, int SPMM_ITEMS, int BITS>
+extern SYCL_EXTERNAL void kspmm_coo_very_sparse_naive(int *max_count, int *max_idx,
+                                 int *offset_rowidx, int *rowidx, int *colidx,
+                                 sycl::half *values, T *B, sycl::half *out,
+                                 float *__restrict__ const dequant_stats,
+                                 int nnz, int rowsA, int rowsB, int colsB,
+                                 const sycl::nd_item<3> &item_ct1,
+                                 sycl::half *smem_dequant_stats);
+
+template <int ITEMS_PER_THREAD, int SUBTILE_ROWS, int THREADS>
+extern SYCL_EXTERNAL void kdequant_mm_int32_fp16(
+    int *__restrict__ const A, float *__restrict__ const rowStats,
+    float *__restrict__ const colStats, sycl::half *out, float *newRowStats,
+    float *newcolStats, sycl::half *__restrict__ const bias, const int numRows,
+    const int numCols, const int tileCols, const int n,
+    const sycl::nd_item<3> &item_ct1, float *smem_rowStats,
+    typename LoadInt32::TempStorage &loadint32,
+    typename ExchangeInt32::TempStorage &exchangeint32);
+
+template<typename T, int THREADS, int ITEMS_PER_THREAD, int TILE_ROWS, int TILE_COLS, int SPARSE_DECOMP> extern SYCL_EXTERNAL void kgetColRowStats(T * __restrict__ A, float *rowStats, float *colStats, int * nnz_count_row, float nnz_threshold, int rows, int cols, int tiledRows, int tiledCols,
+                                                                                                                              const sycl::nd_item<3> &item_ct1,
+                                                                                                                              uint8_t *temp_storage_ct1,
+                                                                                                                              float *smem_row_absmax_values,
+                                                                                                                              int *smem_row_nnz_values);
+template <int THREADS, int ITEMS_PER_THREAD, int TILE_ROWS, int TILE_COLS,
+          int SPARSE_DECOMP>
+extern SYCL_EXTERNAL void kDoubleRowColQuant(sycl::half *__restrict__ const A,
+                        float *__restrict__ const rowStats,
+                        float *__restrict__ const colStats,
+                        char *out_col_normed, char *out_row_normed, int *rowidx,
+                        int *colidx, sycl::half *val,
+                        int *__restrict__ nnz_block_ptr, float threshold,
+                        int rows, int cols, int tiledCols,
+                        const sycl::nd_item<3> &item_ct1,
+                        typename LoadHalf::TempStorage &loadhalf,
+                        typename StoreInt8::TempStorage &storeint8,
+                        float *smem_row_stats, unsigned int *smem_nnz_row_idx);
+
+
+template <int THREADS, int ITEMS_PER_THREAD, int TILE_ROWS, int TILE_COLS, int TRANSPOSE, int FORMAT> extern SYCL_EXTERNAL void kTransformRowToFormat(char *__restrict__ const A, char *out, int rows, int cols, int tiledCols, int outRows, int outCols,
+                                                                                                                                 const sycl::nd_item<3> &item_ct1,
+                                                                                                                                 char *smem_data);
+
+template <int FORMAT> extern SYCL_EXTERNAL  void kExtractOutliers(char *A, int *idx, char *out, int idx_size, int rowsA, int colsA, int tiledRowsA, int tiledColsA,
+                                            const sycl::nd_item<3> &item_ct1);
+
+template <typename T, int BITS, int THREADS> extern SYCL_EXTERNAL void gemm_device(int M, int N, int K, T * __restrict__ const A,  T* B,  T * out,  int lda, int ldb, int ldc);
+template <typename T, int THREADS>  extern SYCL_EXTERNAL void kgemm_4bit_inference(int M, int N, int K, T * __restrict__ const A, unsigned char *B,  float *absmax, T * out,  int lda, int ldb, int ldc, int blocksize);
+template <typename T, int THREADS, int BITS> extern SYCL_EXTERNAL  void kgemm_4bit_inference_naive(int M, int N, int K, T * __restrict__ const A, unsigned char *B,  float *absmax, const float *datatype, T * out,  int lda, int ldb, int ldc, int blocksize,
+                                                                             const sycl::nd_item<3> &item_ct1,
+                                                                             T *quant_map);
+
+template <typename T, int FUNC> extern SYCL_EXTERNAL void kfunc(T *A, T *B, T value, long n,
+                                           const sycl::nd_item<3> &item_ct1);
+
+#endif
---
+++ csrc/ops.dp.hpp
@@ -0,0 +1,215 @@
+// Copyright (c) Facebook, Inc. and its affiliates.
+//
+// This source code is licensed under the MIT license found in the
+// LICENSE file in the root directory of this source tree.
+
+
+#ifndef ops_H
+#define ops_H
+
+#include <oneapi/dpl/execution>
+#include <oneapi/dpl/algorithm>
+#include <sycl/sycl.hpp>
+#include <dpct/dpct.hpp>
+#include <stdio.h>
+#include <iostream>
+#include <unistd.h>
+#include <assert.h>
+#include <dpct/blas_utils.hpp>
+
+#include <dpct/sparse_utils.hpp>
+
+#include <vector>
+#include <functional>
+#include <dpct/dpl_utils.hpp>
+
+
+#define THREADS_PER_BLOCKS (512)
+
+inline void checkCudaStatus(int status) {
+    /*
+    DPCT1000:93: Error handling if-stmt was detected but could not be rewritten.
+    */
+    if (status != 0) {
+        /*
+        DPCT1009:94: SYCL uses exceptions to report errors and does not use the
+        error codes. The original code was commented out and a warning string
+        was inserted. You need to rewrite this code.
+        */
+        printf(
+            "cuda API failed with status %d: %s\n", status,
+            "cudaGetErrorString is not supported" /*cudaGetErrorString(status)*/);
+        /*
+        DPCT1001:92: The statement could not be removed.
+        */
+        throw std::logic_error("cuda API failed");
+    }
+}
+
+inline int checkCublasStatus(int status) {
+    if (status != 0) {
+        printf("cuBLAS API failed with status %d\n", status);
+        //throw std::logic_error("cuBLAS API failed");
+        return 1;
+    }
+    return 0;
+}
+
+typedef enum Operations_t
+{
+	ksmul = 0,
+} Operations_t;
+
+typedef enum Optimizer_t
+{
+	ADAM = 0,
+	MOMENTUM = 1,
+  RMSPROP = 2,
+  LARS = 3,
+  ADAGRAD = 4,
+  LION = 5,
+} Optimizer_t;
+
+typedef enum Transform_t
+{
+	ROW = 0,
+	COL = 1,
+  COL32 = 2,
+  COL_TURING = 3,
+  COL_AMPERE = 4,
+} Transform_t;
+
+typedef enum DataType_t
+{
+	General8bit = 0,
+	FP4 = 1,
+  NF4 = 2,
+} DataType_t;
+
+typedef enum Funcs_t
+{
+	FILL = 0,
+	ARANGE = 1,
+	_MUL = 2,
+} Funcs_t;
+
+class Context
+{
+    public:
+                                dpct::queue_ptr m_handle;
+
+                                Context()
+				{
+                                        dpct::queue_ptr handle;
+                                        handle = &dpct::get_default_queue();
+                                        m_handle = handle;
+				}
+
+};
+
+class ContextLt
+{
+    public:
+                                dpct::queue_ptr m_handle;
+
+                                ContextLt()
+				{
+                                        dpct::queue_ptr handle;
+                                        handle = &dpct::get_default_queue();
+                                        m_handle = handle;
+				}
+
+};
+
+class ContextCusparse
+{
+    public:
+                                sycl::queue *m_handle;
+
+                                ContextCusparse()
+				{
+                                        sycl::queue *handle;
+                                        handle = &dpct::get_default_queue();
+                                        m_handle = handle;
+				}
+
+};
+
+
+template <typename T> void estimateQuantiles(T *A, float *code, float offset, int n);
+
+void quantize(float *code, float *A, unsigned char *out, int n);
+void dequantize(float *code, unsigned char *A, float *out, int n);
+template <typename T, int STOCHASTIC, int DATA_TYPE> void quantizeBlockwise(float * code, T *A, float *absmax, unsigned char *out, float* rand, int rand_offset, int blocksize, const int n);
+template<typename T, int DATA_TYPE> void dequantizeBlockwise(float *code, unsigned char *A, float *absmax, T *out, int block_size, const int n);
+
+template<typename T, int OPTIMIZER> void optimizer32bit(T* g, T* p,
+                float* state1, float* state2, float *unorm, float max_unorm, float param_norm,
+                float beta1, float beta2, float eps, float weight_decay,
+                int step, float lr, const float gnorm_scale, bool skip_zeros, int n);
+
+template<typename T, int OPTIMIZER> void optimizerStatic8bit(T* p, T* g, unsigned char* state1, unsigned char* state2,
+                float *unorm, float max_unorm, float param_norm,
+                float beta1, float beta2,
+                float eps, int step, float lr,
+                float* quantiles1, float* quantiles2,
+                float* max1, float* max2, float* new_max1, float* new_max2,
+                float weight_decay,
+                const float gnorm_scale, int n);
+
+template<typename T, int OPTIMIZER> void optimizerStatic8bitBlockwise(T* p, T* g,
+                unsigned char* state1, unsigned char* state2, float beta1, float beta2, float eps, int step, float lr,
+                float* quantiles1, float* quantiles2, float* absmax1, float* absmax2, float weight_decay, const float gnorm_scale,
+								bool skip_zeros, int n);
+
+template<typename T> void percentileClipping(T * g, float *gnorm_vec, int step, const int n);
+
+extern SYCL_EXTERNAL void histogramScatterAdd2D(float* histogram, int *index1, int *index2, float *src, int maxidx1, int n);
+
+void gemmex(Context * context, bool transposeA, bool transposeB, int m, int n, int k, void *A, void *B, void *C, int lda, int ldb, int ldc);
+void strided_gemmex(Context *context, bool transposeA, bool transposeB, int m, int n, int k, void *A, void *B, void *C, int lda, int ldb, int ldc,
+                    long long int strideA, long long int strideB, long long int strideC, int batchCount);
+
+
+template <int FORMATB, int DTYPE_OUT, int SCALE_ROWS> int igemmlt(dpct::queue_ptr ltHandle, int m, int n, int k, const int8_t *A, const int8_t *B, void *C, float *row_scale, int lda, int ldb, int ldc);
+
+template <typename T, int SRC, int TARGET, bool transpose, int DTYPE> void transform(dpct::queue_ptr ltHandle, T *A, T *out, int dim1, int dim2);
+void cutlass_igemm(bool transposeA, bool transposeB, int m, int n, int k, void *A, void *B, void *C, int lda, int ldb, int ldc);
+void dequant_mm_int32_fp16(int *A, float *rowStats, float *colStats,
+                           sycl::half *out, float *newRowStats,
+                           float *newcolStats, sycl::half *bias, int numRows,
+                           int numCols);
+void getColRowStats(sycl::half *A, float *rowStats, float *colStats,
+                    int *nnz_count_row, float nnz_threshold, int rows,
+                    int cols);
+void doubleRowColQuant(sycl::half *A, float *rowStats, float *colStats,
+                       char *out_col_normed, char *out_row_normed, int *rowidx,
+                       int *colidx, sycl::half *val, int *nnz_block_ptr,
+                       float threshold, int rows, int cols);
+
+template <int FORMAT, int TRANSPOSE> void transformRowToFormat(char * A, char *out, int rows, int cols);
+
+void spmm_coo(sycl::queue *handle, int *A_rowidx, int *A_colidx,
+              sycl::half *A_vals, int A_nnz, int A_rows, int A_cols, int B_cols,
+              int ldb, sycl::half *B, int ldc, sycl::half *C,
+              bool transposed_B);
+
+template <typename T, int BITS>
+void spmm_coo_very_sparse_naive(int *max_count, int *max_idx,
+                                int *offset_rowidx, int *rowidx, int *colidx,
+                                sycl::half *values, T *B, sycl::half *out,
+                                float *dequant_stats, int nnz_rows, int nnz,
+                                int rowsA, int rowsB, int colsB);
+
+template <int FORMAT> void extractOutliers(char * A, int *idx, char *out, int idx_size, int rows, int cols);
+
+void matmul4bite(sycl::half *A, unsigned char *B, sycl::half *out, int lda,
+                 int ldb, int rowsA, int colsA, int colsB);
+
+template <typename T> void gemm_host(int m, int n, int k, T * A,  T* B,  T * out,  int lda, int ldb, int ldc, int bits);
+template <typename T> void gemm_4bit_inference(int m, int n, int k, T * A,  unsigned char* B,  float *absmax, T * out,  int lda, int ldb, int ldc, int blocksize);
+template <typename T, int BITS> void gemm_4bit_inference_naive(int m, int n, int k, T * A,  unsigned char* B,  float *absmax, float *datatype, T * out,  int lda, int ldb, int ldc, int blocksize);
+
+template <typename T, int FUNC> void func(T *A, T *B, T value, long n);
+
+#endif
