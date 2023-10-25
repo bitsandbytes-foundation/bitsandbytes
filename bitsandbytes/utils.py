aceeb1d8ece91a1ce842dcd74f8dd232f13121b3
Titus von Koeller <9048635+Titus-von-Koeller@users.noreply.github.com>
2023-10-25 08:19:23 -0700

    format utils.py

--- bitsandbytes/utils.py
+++ bitsandbytes/utils.py
@@ -1,8 +1,10 @@
 import shlex
 import subprocess
+from typing import Callable, List, Optional, Tuple, Union
+
 import torch
-from typing import List, Optional, Callable, Union, Tuple
-from torch.nn import Module, Linear
+from torch.nn import Linear, Module
+
 
 def outlier_hook(module, input):
     assert isinstance(module, Linear)
@@ -28,7 +30,8 @@ def outlier_hook(module, input):
             # (1) zscore test of std of hidden dimension
             outlier_idx = find_outlier_dims(merged, reduction_dim=1, zscore=3)
             # (2) magnitude > 6 test
-            dims = (torch.abs(input[0])> 6).sum(dim=list(range(len(input[0].shape)-1)))
+            dims = (torch.abs(input[0])
+                    > 6).sum(dim=list(range(len(input[0].shape) - 1)))
             outlier_idx2 = torch.where(dims > 0)[0]
             outlier_idx = torch.cat([outlier_idx, outlier_idx2]).unique()
             tracer.hvalue2outlier_idx[hvalue] = outlier_idx
@@ -37,7 +40,7 @@ def outlier_hook(module, input):
             hook.remove()
 
 
-class OutlierTracer(object):
+class OutlierTracer:
     _instance = None
 
     def __init__(self):
@@ -52,7 +55,7 @@ class OutlierTracer(object):
         self.initialized = True
         self.hooks = []
 
-        for n, m in model.named_modules():
+        for _n, m in model.named_modules():
             if isinstance(m, Linear):
                 self.hooks.append(m.register_forward_pre_hook(outlier_hook))
 
@@ -78,20 +81,22 @@ class OutlierTracer(object):
             cls._instance = cls.__new__(cls)
         return cls._instance
 
+
 def find_outlier_dims(weight, reduction_dim=0, zscore=4.0, topk=None, rdm=False):
     if rdm:
-        return torch.randint(0, weight.shape[1], size=(topk,), device=weight.device).long()
+        return torch.randint(
+            0, weight.shape[1], size=(topk, ), device=weight.device).long()
 
     m = weight.mean(reduction_dim)
     mm = m.mean()
     mstd = m.std()
-    zm = (m-mm)/mstd
+    (m - mm) / mstd
 
     std = weight.std(reduction_dim)
     stdm = std.mean()
     stdstd = std.std()
 
-    zstd = (std-stdm)/stdstd
+    zstd = (std - stdm) / stdstd
 
     if topk is not None:
         val, idx = torch.topk(std.abs(), k=topk, dim=0)
@@ -102,11 +107,10 @@ def find_outlier_dims(weight, reduction_dim=0, zscore=4.0, topk=None, rdm=False)
 
 
 def execute_and_return(command_string: str) -> Tuple[str, str]:
+
     def _decode(subprocess_err_out_tuple):
         return tuple(
-            to_decode.decode("UTF-8").strip()
-            for to_decode in subprocess_err_out_tuple
-        )
+            to_decode.decode("UTF-8").strip() for to_decode in subprocess_err_out_tuple)
 
     def execute_and_return_decoded_std_streams(command_string):
         return _decode(
@@ -114,20 +118,18 @@ def execute_and_return(command_string: str) -> Tuple[str, str]:
                 shlex.split(command_string),
                 stdout=subprocess.PIPE,
                 stderr=subprocess.PIPE,
-            ).communicate()
-        )
+            ).communicate())
 
     std_out, std_err = execute_and_return_decoded_std_streams(command_string)
     return std_out, std_err
 
 
 def replace_linear(
-    model: Module, 
-    linear_replacement: Callable[..., Module], 
-    skip_modules: Union[List[str], Tuple[str, ...]] = ("lm_head",), 
-    copy_weights: bool = False, 
-    post_processing_function: Optional[str] = None
-) -> Module:
+        model: Module,
+        linear_replacement: Callable[..., Module],
+        skip_modules: Union[List[str], Tuple[str, ...]] = ("lm_head", ),
+        copy_weights: bool = False,
+        post_processing_function: Optional[str] = None) -> Module:
     """
     Replace linear modules with a new Linear module.
 
@@ -161,15 +163,16 @@ def replace_linear(
     """
     for name, module in model.named_children():
         if any(isinstance(child, Module) for child in module.children()):
-            replace_linear(module, linear_replacement, skip_modules, copy_weights, post_processing_function)
+            replace_linear(
+                module, linear_replacement, skip_modules, copy_weights,
+                post_processing_function)
 
         if isinstance(module, Linear) and name not in skip_modules:
             old_module = model._modules[name]
             new_module = linear_replacement(
                 old_module.in_features,
                 old_module.out_features,
-                bias=(old_module.bias is not None)
-            )
+                bias=(old_module.bias is not None))
             model._modules[name] = new_module
 
             if copy_weights:
