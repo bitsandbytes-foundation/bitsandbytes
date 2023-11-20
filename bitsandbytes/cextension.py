28b80564b7efbec22eba9800ac4b83ee745a34c1
Prasanth Nunna <Prasanth.nunna@amd.com>
2023-11-20 18:13:42 +0000

    Add default value for HIP_ENVIRONMENT

--- bitsandbytes/cextension.py
+++ bitsandbytes/cextension.py
@@ -29,6 +29,7 @@ try:
     lib.cadam32bit_grad_fp32 # runs on an error if the library could not be found -> COMPILED_WITH_CUDA=False
     lib.get_context.restype = ct.c_void_p
     
+    HIP_ENVIRONMENT = False
     if torch.version.cuda:
         lib.get_cusparse.restype = ct.c_void_p
     elif torch.version.hip:
