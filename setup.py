c75b6ded79e674849f99dff7b55938a8a1f40178
Titus von Koeller <9048635+Titus-von-Koeller@users.noreply.github.com>
2023-12-11 01:46:02 -0800

    postfix release 0.41.3.post1

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ def read(fname):
 
 setup(
     name=f"bitsandbytes",
-    version=f"0.41.3",
+    version=f"0.41.3.post1",
     author="Tim Dettmers",
     author_email="dettmers@cs.washington.edu",
     description="k-bit optimizers and matrix multiplication routines.",
