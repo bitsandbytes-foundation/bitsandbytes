84a6b78bfec4386b620d55c3740f672344f62224
Rickard <rly@combination.se>
2024-01-02 13:49:09 +0100

    Post merge fix errors

--- csrc/mps_ops.mm
+++ csrc/mps_ops.mm
@@ -33,7 +33,7 @@ static inline id<MTLLibrary> get_library()
   NSError *error = nil;  
   static id<MTLLibrary> library = nil;
   if(!library) {
-    library = [get_device() newLibraryWithFile:@"bitsandbytes.metallib" error:&error];
+    library = [get_device() newLibraryWithURL:[NSURL fileURLWithPath:@"bitsandbytes.metallib"] error:&error];
   }
   if(!library) {
     NSLog(@"Failed to load bitsandbytes.metallib");
@@ -62,30 +62,6 @@ extern "C" MPSGraphTensor* quantize_mps(MPSGraph* graph, MPSGraphTensor* code, M
       abort();
     }
   }
-  id<MTLBuffer> codeBuffer = [device newBufferWithBytes:code.data.bytes length:code.data.length options:MTLResourceStorageModeShared];  
-  id<MTLBuffer> ABuffer = [device newBufferWithBytes:A.data.bytes length:A.data.length options:MTLResourceStorageModeShared];  
-  NSUInteger outSize = n * sizeof(unsigned char);  
-  id<MTLBuffer> outBuffer = [device newBufferWithLength:outSize options:MTLResourceStorageModeShared];  
-  id<MTLBuffer> nBuffer = [device newBufferWithBytes:&n length:sizeof(uint) options:MTLResourceStorageModeShared];
-  
-  id<MTLCommandQueue> commandQueue = [device newCommandQueue];  
-  id<MTLCommandBuffer> commandBuffer = [commandQueue commandBuffer];  
-  id<MTLComputeCommandEncoder> commandEncoder = [commandBuffer computeCommandEncoder];  
-  [commandEncoder setComputePipelineState:kernel];  
-  [commandEncoder setBuffer:codeBuffer offset:0 atIndex:0];  
-  [commandEncoder setBuffer:ABuffer offset:0 atIndex:1];  
-  [commandEncoder setBuffer:outBuffer offset:0 atIndex:2];  
-  [commandEncoder setBuffer:nBuffer offset:0 atIndex:3];  
-    
-  int threadsPerThreadgroup = kernel.threadExecutionWidth;  
-  MTLSize threadsPerGrid = MTLSizeMake(n, 1, 1);  
-  MTLSize threadgroupSize = MTLSizeMake(threadsPerThreadgroup, 1, 1);  
-  [commandEncoder dispatchThreads:threadsPerGrid threadsPerThreadgroup:threadgroupSize];  
-    
-  [commandEncoder endEncoding];  
-  [commandBuffer commit];  
-  [commandBuffer waitUntilCompleted];  
-    
-  MPSGraphTensorData* outData = [[MPSGraphTensorData alloc] initWithBytesNoCopy:outBuffer.contents length:outSize deallocator:nil];  
-  return [graph tensorWithTensorData:outData shape:@[@(n)]];
+  NSLog(@"Not implemented");
+  return nil;
 }  
