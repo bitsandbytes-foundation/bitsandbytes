3e69e8fc0cba9b069719367a4d3cb05e07b1a0b0
Zoey <Dessix@Dessix.net>
2024-01-23 20:00:17 +0000

    Bump CUDA 12.2.0 to 12.2.1, fix setup support for Cuda 12.1 (#703), Sort compute capabilities sets to select max
    * Add support for CUDA 12.1
    
    * Update README to include CUDA 12.1 version
    
    * Add support for >= 12x
    
    Co-authored-by: Jeongseok Kang <jskang@lablup.com>
    
    * Temporary version of bitsandbytes PR 527: Sort compute capabilities sets to select max
    
    * Modify PR 506 to support C++20
    
    * Add Cuda 12.2
    
    ---------
    
    Co-authored-by: PriNova <info@prinova.de>
    Co-authored-by: PriNova <31413214+PriNova@users.noreply.github.com>
    Co-authored-by: Jeongseok Kang <jskang@lablup.com>

--- install_cuda.sh
+++ install_cuda.sh
@@ -12,8 +12,8 @@ URL116=https://developer.download.nvidia.com/compute/cuda/11.6.2/local_installer
 URL117=https://developer.download.nvidia.com/compute/cuda/11.7.0/local_installers/cuda_11.7.0_515.43.04_linux.run
 URL118=https://developer.download.nvidia.com/compute/cuda/11.8.0/local_installers/cuda_11.8.0_520.61.05_linux.run
 URL120=https://developer.download.nvidia.com/compute/cuda/12.0.0/local_installers/cuda_12.0.0_525.60.13_linux.run
-URL121=https://developer.download.nvidia.com/compute/cuda/12.1.0/local_installers/cuda_12.1.0_530.30.02_linux.run
-URL122=https://developer.download.nvidia.com/compute/cuda/12.2.0/local_installers/cuda_12.2.0_535.54.03_linux.run
+URL121=https://developer.download.nvidia.com/compute/cuda/12.1.1/local_installers/cuda_12.1.1_530.30.02_linux.run
+URL122=https://developer.download.nvidia.com/compute/cuda/12.2.1/local_installers/cuda_12.2.1_535.86.10_linux.run
 URL123=https://developer.download.nvidia.com/compute/cuda/12.3.1/local_installers/cuda_12.3.1_545.23.08_linux.run
 
 
