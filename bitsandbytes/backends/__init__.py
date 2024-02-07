b933f9f1c686979d6dbf9ea97c753561162459e9
jianan-gu <jianan.gu@intel.com>
2024-02-07 23:00:06 +0800

    refactor base backend registering
    Co-authored-by: Aarni Koskela <akx@iki.fi>

--- bitsandbytes/backends/__init__.py
+++ bitsandbytes/backends/__init__.py
@@ -1,29 +1,14 @@
-from bitsandbytes.cextension import COMPILED_WITH_CUDA
+import typing
 import torch
 
-class Backends:
-    """
-    An dict class for device backends that registered with 8bits and 4bits functions.
+from bitsandbytes.cextension import COMPILED_WITH_CUDA
+from bitsandbytes.backends.base import Backend
 
-    The values of this device backends are lowercase strings, e.g., ``"cuda"``. They can
-    be accessed as attributes with key-value, e.g., ``Backends.device["cuda"]``.
+backends: Dict[str, Backend] = {}
 
-    """
-
-    devices = {}
-
-    @classmethod
-    def register_backend(cls, backend_name: str, backend_instance):
-        assert backend_name.lower() in {
-            "cpu",
-            "cuda",
-            "xpu",
-        }, "register device backend choices in [cpu, cuda, xpu]"
-
-        cls.devices[backend_name.lower()] = backend_instance
+def register_backend(backend_name: str, backend_instance: Backend):
+    backends[backend_name.lower()] = backend_instance
 
 if COMPILED_WITH_CUDA:
     from .cuda import CUDABackend
-    cuda_backend = CUDABackend(torch.device("cuda").type)
-    Backends.register_backend(cuda_backend.get_name(), cuda_backend)
-# TODO: register more backends support
+    register_backend("cuda", CUDABackend())
