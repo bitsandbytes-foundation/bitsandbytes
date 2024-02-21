7ffe552173367c1ee41de5c83a4907b9fae97694
Titus von Koeller <9048635+Titus-von-Koeller@users.noreply.github.com>
2024-02-21 10:43:11 +0000

    ruff format cextension.py

--- bitsandbytes/cextension.py
+++ bitsandbytes/cextension.py
@@ -14,21 +14,27 @@ try:
     if lib is None and torch.cuda.is_available():
         CUDASetup.get_instance().generate_instructions()
         CUDASetup.get_instance().print_log_stack()
-        raise RuntimeError('''
+        raise RuntimeError(
+            """
         CUDA Setup failed despite GPU being available. Please run the following command to get more information:
 
         python -m bitsandbytes
 
         Inspect the output of the command and see if you can locate CUDA libraries. You might need to add them
         to your LD_LIBRARY_PATH. If you suspect a bug, please take the information from python -m bitsandbytes
-        and open an issue at: https://github.com/TimDettmers/bitsandbytes/issues''')
-    _ = lib.cadam32bit_grad_fp32  # runs on an error if the library could not be found -> COMPILED_WITH_CUDA=False
+        and open an issue at: https://github.com/TimDettmers/bitsandbytes/issues"""
+        )
+    _ = (
+        lib.cadam32bit_grad_fp32
+    )  # runs on an error if the library could not be found -> COMPILED_WITH_CUDA=False
     lib.get_context.restype = ct.c_void_p
     lib.get_cusparse.restype = ct.c_void_p
     lib.cget_managed_ptr.restype = ct.c_void_p
     COMPILED_WITH_CUDA = True
 except AttributeError as ex:
-    warn("The installed version of bitsandbytes was compiled without GPU support. "
-        "8-bit optimizers, 8-bit multiplication, and GPU quantization are unavailable.")
+    warn(
+        "The installed version of bitsandbytes was compiled without GPU support. "
+        "8-bit optimizers, 8-bit multiplication, and GPU quantization are unavailable."
+    )
     COMPILED_WITH_CUDA = False
     print(str(ex))
