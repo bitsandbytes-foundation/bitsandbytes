4454fc0f94b938a348f0ae4bff8d09c4fc4d7cdc
Matthew Douglas <38992547+matthewdouglas@users.noreply.github.com>
2024-02-22 20:58:24 -0500

    Migrate from setup.py to pyproject.toml for PEP 660 editable install

--- setup.py
+++ setup.py
@@ -2,19 +2,8 @@
 #
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
-import glob
-import os
-
-from setuptools import find_packages, setup
+from setuptools import setup
 from setuptools.dist import Distribution
-
-libs = list(glob.glob("./bitsandbytes/libbitsandbytes*.*"))
-libs = [os.path.basename(p) for p in libs]
-print("libs:", libs)
-
-
-def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 
 # Tested with wheel v0.29.0
@@ -22,28 +11,7 @@ class BinaryDistribution(Distribution):
     def has_ext_modules(self):
         return True
 
-
 setup(
-    name="bitsandbytes",
     version="0.43.0.dev0",
-    author="Tim Dettmers",
-    author_email="dettmers@cs.washington.edu",
-    description="k-bit optimizers and matrix multiplication routines.",
-    license="MIT",
-    keywords="gpu optimizers optimization 8-bit quantization compression",
-    url="https://github.com/TimDettmers/bitsandbytes",
-    packages=find_packages(),
-    package_data={"": libs},
-    install_requires=["torch", "numpy"],
-    extras_require={
-        "benchmark": ["pandas", "matplotlib"],
-        "test": ["scipy"],
-    },
-    long_description=read("README.md"),
-    long_description_content_type="text/markdown",
-    classifiers=[
-        "Development Status :: 4 - Beta",
-        "Topic :: Scientific/Engineering :: Artificial Intelligence",
-    ],
     distclass=BinaryDistribution,
 )
