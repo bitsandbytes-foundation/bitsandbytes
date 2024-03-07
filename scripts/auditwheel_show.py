d6b1471877ceec086bec9c2513d826a82de85039
Titus von Koeller <9048635+Titus-von-Koeller@users.noreply.github.com>
2024-03-07 18:32:09 +0000

    improve script a bit

--- scripts/auditwheel_show.py
+++ scripts/auditwheel_show.py
@@ -4,27 +4,29 @@ import subprocess
 
 
 def append_to_summary(content):
-    print(content + '\n')  # only for debugging now
+    print(content + "\n")  # only for debugging now
     with open(os.getenv("GITHUB_STEP_SUMMARY"), "a") as summary_file:
         summary_file.write(content + "\n")
 
 
 subprocess.run(["pip", "install", "-q", "auditwheel"])
 
-wheel_files = glob.glob("dist/*.whl")
+wheel_files = glob.glob("**/*.whl", recursive=True)
+
+if not wheel_files:
+    append_to_summary("No wheel files found in `dist/` directory.")
+    exit(0)
 
 for whl in wheel_files:
     append_to_summary("---")
-    append_to_summary("### 🎡 Auditing wheel: `" + whl + "`\n")
+    append_to_summary("### 🎡 Auditing wheel: `" + whl + "`")
 
     audit_wheel_output = subprocess.run(
         ["auditwheel", "show", whl], capture_output=True, text=True
     )
 
     if audit_wheel_output.stdout:
-        append_to_summary(audit_wheel_output.stdout + "\n")
+        append_to_summary(audit_wheel_output.stdout)
 
     if audit_wheel_output.stderr:
-        append_to_summary("**Error:**\n```\n" + audit_wheel_output.stderr + "```\n")
-
-    append_to_summary("\n🐍 **Slithering on to the next one...** 🐍\n")
+        append_to_summary("**Error:**\n```\n" + audit_wheel_output.stderr + "```")
