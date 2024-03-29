288939317ab1d130200697b533d09b3907bc5a6b
Rickard <rickardp@users.noreply.github.com>
2024-03-29 11:07:08 +0100

    Update get_docker_info.py

--- scripts/get_docker_info.py
+++ scripts/get_docker_info.py
@@ -18,12 +18,15 @@ def main():
     ap = argparse.ArgumentParser()
     ap.add_argument("dockerfile")
     ap.add_argument("target")
+    ap.add_argument("-t", "--tag-only", action="store_true")
     ap.add_argument("-v", "--version-only", action="store_true")
     args = ap.parse_args()
-    image = find_image_in_dockerfile(args.dockerfile, args.target)
-    if args.version_only:
-        image = image.rpartition(":")[-1]
-    print(image)
+    result = find_image_in_dockerfile(args.dockerfile, args.target)
+    if args.tag_only or args.version_only:
+        result = result.rpartition(":")[-1]
+        if args.version_only:
+            result = result.split("-")[0]
+    print(result)
 
 if __name__ == '__main__':
     main()
