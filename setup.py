399a49471ffc2b68f03e22cb5359365f49dd16f3
Matthew Douglas <38992547+matthewdouglas@users.noreply.github.com>
2024-04-08 19:36:34 -0400

    Rebase on main

--- setup.py
+++ setup.py
@@ -2,19 +2,8 @@
 #
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
-import glob
-import os
-
-from setuptools import find_packages, setup
+from setuptools import setup
 from setuptools.dist import Distribution
-
-libs = list(glob.glob("./bitsandbytes/libbitsandbytes*.*"))
-libs = [os.path.basename(p) for p in libs]
-print("libs:", libs)
-
-
-def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 
 # Tested with wheel v0.29.0
@@ -24,26 +13,6 @@ class BinaryDistribution(Distribution):
 
 
 setup(
-    name="bitsandbytes",
-    version="0.44.0.dev",
-    author="Tim Dettmers",
-    author_email="dettmers@cs.washington.edu",
-    description="k-bit optimizers and matrix multiplication routines.",
-    license="MIT",
-    keywords="gpu optimizers optimization 8-bit quantization compression",
-    url="https://github.com/TimDettmers/bitsandbytes",
-    packages=find_packages(),
-    package_data={"": libs},
-    install_requires=["torch", "numpy"],
-    extras_require={
-        "benchmark": ["pandas", "matplotlib"],
-        "test": ["scipy"],
-    },
-    long_description=read("README.md"),
-    long_description_content_type="text/markdown",
-    classifiers=[
-        "Development Status :: 4 - Beta",
-        "Topic :: Scientific/Engineering :: Artificial Intelligence",
-    ],
+    version="0.44.0.dev0",
     distclass=BinaryDistribution,
 )
