1f625c874f55e8093b300d18c7c1a53c24a0ebdc
Younes Belkada <younesbelkada@gmail.com>
2024-04-23 14:22:23 +0200

    add  decorator

--- tests/helpers.py
+++ tests/helpers.py
@@ -1,6 +1,7 @@
 from io import BytesIO
 from itertools import product
 import random
+import functools
 from typing import Any, List
 
 import torch
@@ -62,3 +63,40 @@ DTYPE_NAMES = {
 
 def describe_dtype(dtype: torch.dtype) -> str:
     return DTYPE_NAMES.get(dtype) or str(dtype).rpartition(".")[2]
+
+
+# Copied from: https://github.com/huggingface/transformers/blob/2d92db8458f7143f64f9b13cbcfee5eb8d0cab90/src/transformers/testing_utils.py#L2131
+def is_flaky(max_attempts = 5, wait_before_retry = None, description = None):
+    """
+    To decorate flaky tests. They will be retried on failures.
+
+    Args:
+        max_attempts (`int`, *optional*, defaults to 5):
+            The maximum number of attempts to retry the flaky test.
+        wait_before_retry (`float`, *optional*):
+            If provided, will wait that number of seconds before retrying the test.
+        description (`str`, *optional*):
+            A string to describe the situation (what / where / why is flaky, link to GH issue/PR comments, errors,
+            etc.)
+    """
+
+    def decorator(test_func_ref):
+        @functools.wraps(test_func_ref)
+        def wrapper(*args, **kwargs):
+            retry_count = 1
+
+            while retry_count < max_attempts:
+                try:
+                    return test_func_ref(*args, **kwargs)
+
+                except Exception as err:
+                    print(f"Test failed with {err} at try {retry_count}/{max_attempts}.", file=sys.stderr)
+                    if wait_before_retry is not None:
+                        time.sleep(wait_before_retry)
+                    retry_count += 1
+
+            return test_func_ref(*args, **kwargs)
+
+        return wrapper
+
+    return decorator
