9da03f1a07de2a8b658b643c25b4bd0aa266211c
jiqing-feng <jiqing.feng@intel.com>
2024-11-18 15:13:02 +0000

    fix 8bit int8 param device
    Signed-off-by: jiqing-feng <jiqing.feng@intel.com>

--- bitsandbytes/nn/modules.py
+++ bitsandbytes/nn/modules.py
@@ -696,7 +696,7 @@ class Int8Params(torch.nn.Parameter):
         elif device.type == "xpu":
             if self.data.dtype == torch.int8:
                 self.CB = self.data
-                return self.to(device="xpu", non_blocking=non_blocking)
+                return super().xpu(device)
             else:
                 return self.xpu()
         else:
