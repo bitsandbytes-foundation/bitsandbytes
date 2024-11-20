cd73601fcb70f83f663b71c0169548facba3cd06
Huazhong Ji <hzji210@gmail.com>
2024-11-20 20:24:45 +0800

    Fix issue that no valid semantic version tag found when installing bitsandbytes from source in personal repo (#1419)

--- setup.py
+++ setup.py
@@ -27,7 +27,8 @@ def get_latest_semver_tag():
     tags = subprocess.check_output(["git", "tag"], text=True).splitlines()
     semver_tags = [tag for tag in tags if tag.count(".") == 2 and all(part.isdigit() for part in tag.split("."))]
     if not semver_tags:
-        raise ValueError("No valid semantic version tags found")
+        print("No valid semantic version tags found, use 0.0.1 defaultly")
+        semver_tags = ["0.0.1"]
     return sorted(semver_tags, key=lambda s: list(map(int, s.split("."))))[-1]
 
 
