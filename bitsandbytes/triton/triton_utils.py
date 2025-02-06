66da99aad10c41fb9fd70cdacf6db561ac67dfb7
Matthew Douglas <38992547+matthewdouglas@users.noreply.github.com>
2025-02-06 12:30:23 -0500

    Improve guard for triton compatibility (#1497)

--- bitsandbytes/triton/triton_utils.py
+++ bitsandbytes/triton/triton_utils.py
@@ -1,5 +1,14 @@
-import importlib
+import functools
 
 
+@functools.lru_cache(None)
 def is_triton_available():
-    return importlib.util.find_spec("triton") is not None
+    try:
+        # torch>=2.2.0
+        from torch.utils._triton import has_triton, has_triton_package
+
+        return has_triton_package() and has_triton()
+    except ImportError:
+        from torch._inductor.utils import has_triton
+
+        return has_triton()
