0c1d9fc7c3851e7e266fcd66ecc0ba7b3231f589
Matthew Douglas <38992547+matthewdouglas@users.noreply.github.com>
2025-05-02 15:08:59 -0400

    Update build cpu script

--- .github/scripts/build-cpu.sh
+++ .github/scripts/build-cpu.sh
@@ -6,12 +6,7 @@ set -xeuo pipefail
 
 pip install cmake==3.28.3
 
-if [ "${build_os:0:6}" == ubuntu ] && [ "${build_arch}" == aarch64 ]; then
-	# Allow cross-compile on aarch64
-	sudo apt-get update
-	sudo apt-get install -y gcc-aarch64-linux-gnu binutils-aarch64-linux-gnu g++-aarch64-linux-gnu
-	cmake -DCMAKE_C_COMPILER=aarch64-linux-gnu-gcc -DCMAKE_CXX_COMPILER=aarch64-linux-gnu-g++ -DCOMPUTE_BACKEND=cpu .
-elif [ "${build_os:0:5}" == macos ] && [ "${build_arch}" == aarch64 ]; then
+if [ "${build_os:0:5}" == macos ] && [ "${build_arch}" == aarch64 ]; then
 	cmake -DCMAKE_OSX_ARCHITECTURES=arm64 -DCOMPUTE_BACKEND=cpu .
 else
 	cmake -DCOMPUTE_BACKEND=cpu .
