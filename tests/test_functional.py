959a0d42f54894dc06f0daf0c3e251af8d8dae09
jiqing-feng <jiqing.feng@intel.com>
2025-05-12 13:58:06 +0000

    skip not used function teests
    Signed-off-by: jiqing-feng <jiqing.feng@intel.com>

--- tests/test_functional.py
+++ tests/test_functional.py
@@ -97,6 +97,8 @@ class Test8BitBlockwiseQuantizeFunctional:
         if device in ("cpu", "xpu"):
             if blocksize != 256:
                 pytest.skip("Only blocksize 256 is used in CPU/XPU")
+            if dtype != torch.float32:
+                pytest.skip("Only float32 is used in CPU/XPU")
 
         diffs = []
         reldiffs = []
@@ -130,10 +132,11 @@ class Test8BitBlockwiseQuantizeFunctional:
         abserr = sum(diffs) / len(diffs)
         relerr = sum(reldiffs) / len(reldiffs)
         if signed:
+            threshold_abserr = 0.0036 if device in ("cpu", "xpu") else 0.0035
             assert abserr < 0.0036
             assert relerr < 0.015
         else:
-            assert abserr < 0.0023
+            assert abserr < 0.00175 if device in ("cpu", "xpu") else 0.0023
             assert relerr < 0.012
         assert A2.dtype == dtype
         # print('signed=', signed, 'nested=', nested, 'rand', blocksize, sum(diffs)/len(diffs))
