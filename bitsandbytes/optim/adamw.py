b4de8c57ef8a43c5a12d7099774436fa7a3825ae
V-E-D <vedantthote2019@gmail.com>
2025-05-28 23:30:15 +0530

    precommit

--- bitsandbytes/optim/adamw.py
+++ bitsandbytes/optim/adamw.py
@@ -2,8 +2,8 @@
 #
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
+
 from bitsandbytes.optim.optimizer import Optimizer2State
-import warnings
 
 
 class AdamW(Optimizer2State):
