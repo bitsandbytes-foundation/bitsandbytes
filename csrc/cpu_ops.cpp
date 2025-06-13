4955d136ae083c2be1236d8915913166e1790aad
Matthew Douglas <38992547+matthewdouglas@users.noreply.github.com>
2025-06-13 12:27:59 -0400

    Apply clang-format rules (#1678)

--- csrc/cpu_ops.cpp
+++ csrc/cpu_ops.cpp
@@ -4,7 +4,7 @@
 
 using namespace BinSearch;
 
-void dequantize_cpu(float *code, unsigned char *A, float *absmax, float *out, long long blocksize, long long n) {
+void dequantize_cpu(float* code, unsigned char* A, float* absmax, float* out, long long blocksize, long long n) {
     for (long long block_idx = 0; block_idx < n; block_idx += blocksize) {
         long long valid_items = n - block_idx >= blocksize ? blocksize : n - block_idx;
         long long block_end = block_idx + valid_items;
@@ -13,8 +13,7 @@ void dequantize_cpu(float *code, unsigned char *A, float *absmax, float *out, lo
     }
 }
 
-void quantize_cpu(float *code, float *A, float *absmax, unsigned char *out, long long blocksize, long long n)
-{
+void quantize_cpu(float* code, float* A, float* absmax, unsigned char* out, long long blocksize, long long n) {
 
     // the default code is has range [-0.993, 1.0] which can cause an error in the binary search algorithm used below
     code[0] = -1.0f;
@@ -28,36 +27,35 @@ void quantize_cpu(float *code, float *A, float *absmax, unsigned char *out, long
     int thread_wave_size = 256;
     // we chunk the threads into waves of 256 since the max limit is
     // between 16k and 64k on Linux (we reach this when running BLOOM-176B with a large batch size)
-    for(long long offset = 0; offset < num_blocks; offset+=thread_wave_size)
-    {
-      long long valid_chunks = num_blocks - offset >= thread_wave_size ? thread_wave_size : num_blocks - offset;
-      std::vector<std::thread> threads(valid_chunks);
-      std::vector<quantize_block_args> args(valid_chunks);
+    for (long long offset = 0; offset < num_blocks; offset += thread_wave_size) {
+        long long valid_chunks = num_blocks - offset >= thread_wave_size ? thread_wave_size : num_blocks - offset;
+        std::vector<std::thread> threads(valid_chunks);
+        std::vector<quantize_block_args> args(valid_chunks);
 
-      int chunks_processed = 0;
-      for(long long block_idx = offset*blocksize; block_idx < n; block_idx += blocksize)
-      {
-          long long valid_items = n - block_idx >= blocksize ? blocksize : n - block_idx;
-          long long block_end = block_idx + valid_items;
+        int chunks_processed = 0;
+        for (long long block_idx = offset * blocksize; block_idx < n; block_idx += blocksize) {
+            long long valid_items = n - block_idx >= blocksize ? blocksize : n - block_idx;
+            long long block_end = block_idx + valid_items;
 
-          struct quantize_block_args& arg = args[chunks_processed];
-          arg.bin_searcher = &bin_searcher;
-          arg.code = code;
-          arg.A = A;
-          arg.absmax = absmax;
-          arg.out = out;
-          arg.block_end = block_end;
-          arg.block_idx = block_idx;
-          arg.threadidx = block_idx / blocksize;
-          arg.blocksize = blocksize;
+            struct quantize_block_args& arg = args[chunks_processed];
+            arg.bin_searcher = &bin_searcher;
+            arg.code = code;
+            arg.A = A;
+            arg.absmax = absmax;
+            arg.out = out;
+            arg.block_end = block_end;
+            arg.block_idx = block_idx;
+            arg.threadidx = block_idx / blocksize;
+            arg.blocksize = blocksize;
 
-          threads[chunks_processed] = std::thread([arg] { quantize_block(arg); });
-          chunks_processed += 1;
-          if(chunks_processed == valid_chunks){ break; }
-      }
+            threads[chunks_processed] = std::thread([arg] { quantize_block(arg); });
+            chunks_processed += 1;
+            if (chunks_processed == valid_chunks) {
+                break;
+            }
+        }
 
-      for (int i = 0; i < valid_chunks; i++)
-          threads[i].join();
+        for (int i = 0; i < valid_chunks; i++)
+            threads[i].join();
     }
-
 }
