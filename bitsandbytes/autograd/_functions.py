e5cf8214f7186ea7ae030c659fa05e0fd3d7bed6
jiqing-feng <jiqing.feng@intel.com>
2025-06-20 13:28:40 +0000

    fix test_module
    Signed-off-by: jiqing-feng <jiqing.feng@intel.com>

--- bitsandbytes/autograd/_functions.py
+++ bitsandbytes/autograd/_functions.py
@@ -319,8 +319,6 @@ class MatMul8bitFp(torch.autograd.Function):
 
         CB = state.CB.data.to(A.dtype).mul_(state.SCB.unsqueeze(1).mul(1.0 / 127.0))
         output = torch.nn.functional.linear(A, CB, bias)
-        # to pass the test: tests/test_modules.py::test_linear8bitlt_no_fp16_weights[2.0-xpu]
-        state.idx = False
         ctx.state = state
         ctx.dtype_A = A.dtype
         ctx.grad_shape = A.shape
