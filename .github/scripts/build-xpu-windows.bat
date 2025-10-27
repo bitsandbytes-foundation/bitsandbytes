3f8977c56c2bd839fb405c5112d4f961e0d09707
Matthew Douglas <38992547+matthewdouglas@users.noreply.github.com>
2025-10-27 12:29:17 -0400

    Update XPU Windows script

--- .github/scripts/build-xpu-windows.bat
+++ .github/scripts/build-xpu-windows.bat
@@ -2,13 +2,15 @@
 set INTEL_DLE_URL=https://registrationcenter-download.intel.com/akdlm/IRC_NAS/75d4eb97-914a-4a95-852c-7b9733d80f74/intel-deep-learning-essentials-2025.1.3.8_offline.exe
 
 curl -o intel-dle-installer.exe %INTEL_DLE_URL%
-start /wait "Intel DLE Install" intel-dle-installer.exe --eula=accept --silent
+start /wait "Intel DLE Install" intel-dle-installer.exe -r -a --eula=accept -p=NEED_VS2022_INTEGRATION=0 > intel_dle_log.txt 2>&1
+type intel_dle_log.txt
+
 if ERRORLEVEL 1 (
     echo Failed to install Intel Deep Learning Essentials
     exit /b 1
 )
 
-call %ProgramFiles(x86)%\Intel\oneAPI\setvars.bat
+call "%ProgramFiles(x86)%\Intel\oneAPI\setvars.bat"
 if ERRORLEVEL 1 (
     echo Failed to setup environment
     exit /b 1
