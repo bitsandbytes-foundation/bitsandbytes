55cbaa0d0809711b1df1b5bb44da49312bc535d0
jiqing-feng <jiqing.feng@intel.com>
2025-10-29 09:46:18 +0000

    use bf16 on CPU
    Signed-off-by: jiqing-feng <jiqing.feng@intel.com>

--- bitsandbytes/autograd/_functions.py
+++ bitsandbytes/autograd/_functions.py
@@ -432,6 +432,9 @@ def matmul_4bit(
     bias: Optional[torch.Tensor] = None,
 ):
     assert quant_state is not None
+    # Change dtype to bfloat16 on CPU
+    if A.device.type == "cpu" and quant_state.dtype == torch.float32:
+        quant_state.dtype = torch.bfloat16
 
     if A.numel() == A.shape[-1] and A.requires_grad == False and A.device.type != "hpu":
         if A.shape[-1] % quant_state.blocksize != 0:
