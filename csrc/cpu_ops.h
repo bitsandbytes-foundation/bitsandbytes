dea8dd6377d788aaccabccc350d343c3a41d114f
jiqing-feng <jiqing.feng@intel.com>
2025-10-30 15:31:55 +0000

    enable float to bf16
    Signed-off-by: jiqing-feng <jiqing.feng@intel.com>

--- csrc/cpu_ops.h
+++ csrc/cpu_ops.h
@@ -3,6 +3,9 @@
 
 #include <iostream>
 #include <stdio.h>
+#include <cstdint>
+#include <cstring>
+#include <type_traits>
 
 void quantize_cpu(float* code, float* A, float* absmax, unsigned char* out, long long blocksize, long long n);
 
@@ -17,6 +20,13 @@ using fp16_t = _Float16;
 struct bf16_t {
     uint16_t v;
 };
+
+static inline bf16_t float_to_bf16(float x) {
+    uint32_t bits;
+    std::memcpy(&bits, &x, 4);
+    uint32_t r = bits + 0x7FFF + ((bits >> 16) & 1);
+    return bf16_t{static_cast<uint16_t>(r >> 16)};
+}
 
 template <typename T, int DATA_TYPE>
 void dequantizeBlockwiseCpu(float* code, unsigned char* A, float* absmax, T* out, long long blocksize, long long n);
