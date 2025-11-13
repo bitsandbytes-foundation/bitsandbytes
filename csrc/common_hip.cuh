7f41f0e053a1fd8805c8b961f54a15522e5d47bb
Matthew Douglas <38992547+matthewdouglas@users.noreply.github.com>
2025-11-13 10:33:18 -0500

    Apply suggestion from @matthewdouglas

--- csrc/common_hip.cuh
+++ csrc/common_hip.cuh
@@ -1,9 +1,9 @@
 #pragma once
 
 #ifdef __GFX9__
-    #define BNB_WARP_SIZE 64
+#define BNB_WARP_SIZE 64
 #else 
-    #define BNB_WARP_SIZE 32
+#define BNB_WARP_SIZE 32
 #endif
 
 // These are set based on current BNB support for CDNA 2 & RDNA 3. Update as needed for future archs
