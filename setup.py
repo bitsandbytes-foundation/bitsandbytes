185180a689bf68122b578caa21f7f5445e7dd843
Matthew Douglas <38992547+matthewdouglas@users.noreply.github.com>
2025-11-13 12:19:18 -0500

    CI: skip rebuilding CPU lib when building/installing wheels

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 from distutils.errors import DistutilsModuleError
+import os
 from warnings import warn
 
 from setuptools import find_packages, setup
@@ -18,24 +19,31 @@ class BinaryDistribution(Distribution):
 
 class ExtBuildPy(build_py):
     def run(self):
-        # build_cmake needs to be called prior to build_py, as the latter
-        # collects the files output into the package directory.
-        try:
-            self.run_command("build_cmake")
-        except DistutilsModuleError:
-            warn(
-                "scikit-build-core not installed, CMake will not be invoked automatically. "
-                "Please install scikit-build-core or run CMake manually to build extensions."
-            )
+        if os.environ.get("BNB_SKIP_CMAKE", "").lower() in ("1", "true", "yes"):
+            print("skipping CMake build")
+        else:
+            # build_cmake needs to be called prior to build_py, as the latter
+            # collects the files output into the package directory.
+            try:
+                self.run_command("build_cmake")
+            except DistutilsModuleError:
+                warn(
+                    "scikit-build-core not installed, CMake will not be invoked automatically. "
+                    "Please install scikit-build-core or run CMake manually to build extensions."
+                )
         super().run()
 
 
-setup(
-    version="0.49.0.dev0",
-    packages=find_packages(),
-    distclass=BinaryDistribution,
-    cmake_source_dir=".",
-    cmdclass={
-        "build_py": ExtBuildPy,
-    },
-)
+cmdclass = {"build_py": ExtBuildPy}
+
+setup_kwargs = {
+    "version": "0.49.0.dev0",
+    "packages": find_packages(),
+    "distclass": BinaryDistribution,
+    "cmdclass": {"build_py": ExtBuildPy},
+}
+
+if os.environ.get("BNB_SKIP_CMAKE", "").lower() not in ("1", "true", "yes"):
+    setup_kwargs["cmake_source_dir"] = "."
+
+setup(**setup_kwargs)
