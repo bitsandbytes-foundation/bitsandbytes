2a58fe07dd8e86e5bf9ffbbfcafcec8b75411838
Matthew Douglas <38992547+matthewdouglas@users.noreply.github.com>
2025-12-02 16:29:07 -0500

    ROCm: reduce size of builds

--- .github/scripts/build-rocm.sh
+++ .github/scripts/build-rocm.sh
@@ -19,7 +19,7 @@ if [ "${build_os:0:6}" == ubuntu ]; then
         -w /src -v "$PWD:/src" "$image" sh -c \
         "apt-get update \
       && pip install cmake==3.31.6 \
-      && cmake -DCOMPUTE_BACKEND=hip -DBNB_ROCM_ARCH=\"${bnb_rocm_arch}\" . \
+      && cmake -DCOMPUTE_BACKEND=hip -DCMAKE_BUILD_TYPE=MinSizeRel -DCMAKE_HIP_FLAGS=\"--offload-compress\" -DBNB_ROCM_ARCH=\"${bnb_rocm_arch}\" . \
       && cmake --build ."
 fi
 
