4ab4415949a04f8fbc6926fe979c2a6137674244
Aarni Koskela <akx@iki.fi>
2026-01-14 10:39:11 +0200

    CI: use uv

--- .github/scripts/build-rocm.sh
+++ .github/scripts/build-rocm.sh
@@ -18,7 +18,7 @@ if [ "${build_os:0:6}" == ubuntu ]; then
     docker run --rm --platform "linux/$build_arch" -i \
         -w /src -v "$PWD:/src" "$image" sh -c \
         "apt-get update \
-      && pip install cmake==3.31.6 \
+      && uv tool install cmake==3.31.6 \
       && cmake -DCOMPUTE_BACKEND=hip -DCMAKE_BUILD_TYPE=MinSizeRel -DCMAKE_HIP_FLAGS=\"--offload-compress\" -DBNB_ROCM_ARCH=\"${bnb_rocm_arch}\" . \
       && cmake --build ."
 fi
