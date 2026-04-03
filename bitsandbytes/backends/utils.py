29826d98ed1f3e4c90c60bddd55925d305e1f7ac
Shaun Walbridge <shaun.walbridge@gmail.com>
2026-04-03 12:15:31 -0400

    use direct import rather than transitive

--- bitsandbytes/backends/utils.py
+++ bitsandbytes/backends/utils.py
@@ -1,4 +1,4 @@
-import importlib
+from importlib.metadata import metadata
 
 from packaging import version
 import torch
@@ -69,7 +69,7 @@ def get_gaudi_sw_version():
     """
     try:
         # if we find the spec, examine the installed version
-        plugin_metadata = importlib.metadata.metadata("habana-torch-plugin")
+        plugin_metadata = metadata("habana-torch-plugin")
         plugin_version = plugin_metadata.get("Version")
         if plugin_version:
             gaudi_version = version.parse(plugin_version)
